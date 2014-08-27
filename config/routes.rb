9dba39a67723b626696d38d8ff23ee618faf8dd0
Sherry Seibel <sherry.seibel@gmail.com>
2014-08-27 10:53:25 -0400

    Remove stories#index route
    The stories#index template is no longer being used

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resource :home, only: [:index]
   devise_for :users
   resource :user, only: [:show, :edit, :update]
-  resources :stories, only: [:index, :new, :create]
+  resources :stories, only: [:new, :create]
 
   root to: "homes#index"
 end
