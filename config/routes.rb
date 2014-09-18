d41b49b1311c37ab9815cb8fe69d641718c95565
Sherry Seibel <sherry.seibel@gmail.com>
2014-09-18 17:20:19 -0400

    Update routes for users
    resource :user was changed to resources :user to fix the URL bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resource :home, only: [:index]
   devise_for :users
-  resource :user, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update]
   resources :stories, only: [:new, :create, :destroy]
 
   root to: "homes#index"
