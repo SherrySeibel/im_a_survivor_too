b1c811f13ce14bb07b6c7de122241e403b16de1f
Sherry Seibel <sherry.seibel@gmail.com>
2014-09-26 11:26:51 -0400

    Update user routing
    Change 'resource :user' to resources :users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resource :home, only: [:index]
   devise_for :users
-  resource :user, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update]
   resources :stories, only: [:new, :create]
 
   root to: "homes#index"
