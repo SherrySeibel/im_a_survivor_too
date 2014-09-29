986a45321c7cc4ddb8db3712b56d9a73c7a7db36
Sherry Seibel <sherry.seibel@gmail.com>
2014-09-29 16:18:33 -0400

    Remove variable assignment to video and photo in stories#destroy

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -20,8 +20,8 @@ class StoriesController < ApplicationController
   end
 
   def destroy
-    photo = params[:type] == "photo"
-    video = params[:type] == "video"
+    photo
+    video
     object = Story.find(params[:id])
     object.destroy
 
